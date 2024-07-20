1b60f90915be4afd903ae0960eb32b446db6b9a4
Ashley <ashley.conejomolina@ontariotechu.net>
2024-07-20 02:58:21 -0400

    Splitting Arrays

--- A2.py
+++ A2.py
@@ -8,4 +8,35 @@ class Merge_Sort_A():
 
     def array_maximun_randomize(self,maximun_element):
         for _ in range (maximun_element):
-            self.Arrayempty.append(random.randint(1,maximun_element))  
+            self.Arrayempty.append(random.randint(1,maximun_element)) 
+
+    def split_Arrays(self, Arrayempty= None):
+
+        if Arrayempty is None:
+            Arrayempty = self.Arrayempty
+
+        if len(Arrayempty) <= 1:
+
+            return Arrayempty
+        
+        halfpoint = len(Arrayempty)//2
+        self.sounds_for_sort()
+        print("Half point position: ", halfpoint)
+
+        left_half = Arrayempty[:halfpoint]
+        self.sounds_for_sort()
+        print("Left Half", left_half)
+       
+        right_half = Arrayempty[halfpoint:]
+        self.sounds_for_sort()
+        print("Right Half", right_half)
+        
+        split_left = self.split_Arrays(left_half)
+        self.sounds_for_sort()
+        print("Splilt Left ", split_left)
+        
+        split_right = self.split_Arrays(right_half)
+        self.sounds_for_sort()
+        print("Split Right ", split_right)
+        
+        return self.merge_sort(split_left, split_right)
